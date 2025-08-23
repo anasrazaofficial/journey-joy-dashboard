d22e552f54df148a901c6159c767333e76e90655
anasrazaofficial <anasbukhari4114@gmail.com>
2025-08-24 00:25:29 +0500

    Install tailwind

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
-import img_avatar from './avatar.png';
+import img_avatar from "./avatar.png";
+import img_dashboardChart from "./dashboard-chart.png";
 import img_dashboardStatCardRevenue from "./dashboard-stat-card-revenue.svg";
 import img_dashboardStatCardSubscription from "./dashboard-stat-card-revenue.svg";
 import img_logo from "./logo.svg";
@@ -7,6 +8,7 @@ import img_logout from "./logout.svg";
 
 export const
     avatar = img_avatar,
+    dashboardChart = img_dashboardChart,
     dashboardStatCardRevenue = img_dashboardStatCardRevenue,
     dashboardStatCardSubscription = img_dashboardStatCardSubscription,
     logo = img_logo,
