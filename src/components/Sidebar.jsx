36d7de37cb39a3417c346ae61f5a675d0f3f0d55
anasrazaofficial <anasbukhari4114@gmail.com>
2025-08-24 22:18:26 +0500

    Configure active link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { logo, logout } from "../assets";
 import { SidebarCollege, SidebarDashboard, SidebarPassengerMan, SidebarRegion, SidebarSubscription } from "../components";
 
@@ -6,8 +6,9 @@ import { SidebarCollege, SidebarDashboard, SidebarPassengerMan, SidebarRegion, S
 export const Sidebar = () => {
     return (
         <aside className="w-64 h-dvh bg-[#2C2C2C] fixed top-0 left-0 py-8 overflow-y-auto">
-            <img src={logo} alt="Logo" height={83} width={130} className="mx-auto block" />
-
+            <Link to="/">
+                <img src={logo} alt="Logo" height={83} width={130} className="mx-auto block" />
+            </Link>
 
 
 
@@ -16,74 +17,90 @@ export const Sidebar = () => {
 
             <ul className="nav-links mt-[59px] text-lg text-white/75">
                 <li>
-                    <NavLink to="/"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarDashboard />
-                        <span>Dashboard</span>
+                    <NavLink to="/" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarDashboard className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Dashboard</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/passenger-managment"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarPassengerMan />
-                        <span>Passenger Management</span>
+                    <NavLink to="/passenger-managment" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarPassengerMan className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Passenger Management</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/region-management"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarRegion />
-                        <span>Region Management</span>
+                    <NavLink to="/region-management" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarRegion className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Region Management</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/colleges-and-universities"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarCollege />
-                        <span>Colleges & Universities</span>
+                    <NavLink to="/colleges-and-universities" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarCollege className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Colleges & Universities</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/subscription-management"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarSubscription />
-                        <span>Subscription Management</span>
+                    <NavLink to="/subscription-management" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarSubscription className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Subscription Management</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/fares-management"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarDashboard />
-                        <span>Fares Management</span>
+                    <NavLink to="/fares-management" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarDashboard className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Fares Management</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/driver-management"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarPassengerMan />
-                        <span>Driver Management</span>
+                    <NavLink to="/driver-management" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarPassengerMan className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Driver Management</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
 
                 <li>
-                    <NavLink to="/driver-performance-payout"
-                        className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}
-                    >
-                        <SidebarPassengerMan />
-                        <span>Driver Performance & Payout</span>
+                    <NavLink to="/driver-performance-payout" className={({ isActive }) => `group ${isActive ? "bg-[#444444] text-white" : ""}`}>
+                        {({ isActive }) => (
+                            <>
+                                <SidebarPassengerMan className={isActive ? "!fill-[#FFEC3D]" : ""} />
+                                <span>Driver Performance & Payout</span>
+                            </>
+                        )}
                     </NavLink>
                 </li>
             </ul>
